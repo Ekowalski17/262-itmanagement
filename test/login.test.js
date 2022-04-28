4fc7acf2def4cb2e04572c2889c335b7a1aab0b0
Elizabeth Davis <elizabethdavis@Elizabeths-MacBook-Pro.local>
2022-04-28 11:24:55 -0600

    added token tests

---
+++ test/login.test.js
@@ -0,0 +1,13 @@
+import {login} from "../utils/login.js";
+it('Should get login token', async ()=> {
+    const loginRequest = {
+        userName: "cupcakes123@gmail.com",
+        password: "Cupcakesarebadforyou_22$"
+    }
+    const token = await login(loginRequest);
+    console.log ("Token" + token)
+
+
+
+})
+
